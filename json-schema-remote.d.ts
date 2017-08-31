995561eaff85cc4c44b269fa16bf4e30f4f1a4c1
Simon Scherzinger <scherzinger@entrecode.de>
2017-08-31 18:12:21 +0200

    version 1.3.0 with typings

---
+++ json-schema-remote.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for json-schema-remote
+// Project: json-schema-remote
+// Definitions by: Simon Scherzinger <scherzinger@entrecode.de>
+
+declare module 'json-schema-remote' {
+  export interface Validator {
+    validate: (dataOrURL: string | any, schemaOrURL: string | any, callback?: (error: Error, isValid: boolean) => void) => Promise<boolean>,
+    preload: (url: string, schema: any) => void,
+    getSchema: (url: string) => any,
+    setLoggingFunction: (fn: (...any: Array<string>) => void) => void,
+  }
+
+  export function validate(dataOrURL: string | any, schemaOrURL: string | any, callback?: (error: Error, isValid: boolean) => void): Promise<boolean>;
+
+  export function preload(url: string, schema: any): void;
+
+  function getSchema(url: string): any;
+
+  export function setLoggingFunction(fn: (...any: Array<string>) => void): void
+}
